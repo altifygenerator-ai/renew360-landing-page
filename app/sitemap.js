0e32bfda3ded4655c5969dc284114c225bb2a8d9
J H <altifygenerator@gmail.com>
2026-07-14 13:55:16 -0500

    fix

---
+++ app/sitemap.js
@@ -0,0 +1,12 @@
+﻿const siteUrl = 'https://www.renew360cleaning.com';
+
+export default function sitemap() {
+  return [
+    {
+      url: siteUrl,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ];
+}
